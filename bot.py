6d1c2735cda46711a48e3af16b457156b82c994d
Allen Davis-Swing <allen.davisswing@gmail.com>
2020-11-07 18:08:59 -0500

    here

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import os
 import discord
 import datetime
+from datetime import timedelta
 import pickle
 import os.path
 from googleapiclient.discovery import build
@@ -10,71 +11,57 @@ from google.auth.transport.requests import Request
 from dotenv import load_dotenv
 
 # If modifying these scopes, delete the file token.pickle.
-<<<<<<< Updated upstream
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
-=======
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 emmett = 'sob00kp7sf2l7rf2cskulendto@group.calendar.google.com'
 allen = '87aalj3tmll3n7qaq95ib60m0o@group.calendar.google.com'
 sidhu = 'j05ajvtilvu618f16j9msce9gc@group.calendar.google.com'
->>>>>>> Stashed changes
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
 client = discord.Client()
+
+creds = None
+if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'cred.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
+
+service = build('calendar', 'v3', credentials=creds)
+
+# Call the Calendar API
+now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+print('Getting the upcoming 10 events')
+events_result = service.events().list(calendarId='primary', timeMin=now,
+                                    maxResults=10, singleEvents=True,
+                                    orderBy='startTime').execute()
+events = events_result.get('items', [])
+
+if not events:
+    print('No upcoming events found.')
+for event in events:
+    start = event['start'].get('dateTime', event['start'].get('date'))
+    print(start, event['summary'])
 
 @client.event
 async def on_ready():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'cred.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('calendar', 'v3', credentials=creds)
-
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=10, singleEvents=True,
-                                        orderBy='startTime').execute()
-    events = events_result.get('items', [])
-
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+    return
+    
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     if message.content == '!availablenow':
-<<<<<<< Updated upstream
-        await message.channel.send('exec')
-    if message.content == '!stop': await client.logout()
-
-
-=======
         free = await people_now(message)
         for name in free:
             await message.channel.send(name)
@@ -86,15 +73,18 @@ async def on_message(message):
 @client.event
 async def people_now(message):
     output = ['These people are available: ']
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    events_result = service.events().list(calendarId=emmett, timeMin=now,
-                                        maxResults=1, singleEvents=True,
-                                        orderBy='startTime').execute()
+    now = datetime.datetime.now()
+    before = now - timedelta(minutes=1)
+    after = now + timedelta(minutes=1)
+    before = before.isoformat()
+    after = after.isoformat()
+
+    events_result = service.events().list(calendarId=sidhu, timeMin=before, timeMax = after, maxResults=1, singleEvents=True).execute()
     events = events_result.get('items', [])
 
     if not events:
-        output.append('allen')
+        output.append('sidhu')
+
     return output
 
->>>>>>> Stashed changes
 client.run(TOKEN)
